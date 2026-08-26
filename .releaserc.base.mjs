2793bf8234155c596693751ab2b67b08d4a5f2aa
Amirreza rahmani <163791912+amir-rahmanii@users.noreply.github.com>
2026-08-26 16:27:35 +0330

    chore: add semantic-release
    * chore: add semantic-release
    
    * set prepare only for workspace
    
    ---------
    
    Co-authored-by: Amirabbas <amirabbasjalali82@gmail.com>

---
+++ .releaserc.base.mjs
@@ -0,0 +1,12 @@
+export default {
+  extends: 'semantic-release-monorepo',
+
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
+
+  preset: 'angular',
+};
